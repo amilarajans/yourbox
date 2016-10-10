600f41c6787138e514b2f62b3dbc2fdcb6894ca7
Amila Kumara <amilarajans@gmail.com>
2016-10-10 16:00:54 +0530

    initial commit

---
+++ src/main/java/com/origins/YourBoxApplication.java
@@ -0,0 +1,12 @@
+package com.origins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourBoxApplication.class, args);
+    }
+}
